5d70d3f2c4140aae33b6cafc147cd8ed8d8fee40
armod <armods@gmail.com>
2023-01-27 14:50:24 +0100

    update

--- cuvalley-hack-2023/src/components/Footer.js
+++ cuvalley-hack-2023/src/components/Footer.js
@@ -2,7 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Footer = () => {
-  return <Wrapper>Footer</Wrapper>
+  return (
+    <Wrapper>
+      <h5>
+        &copy; {new Date().getFullYear()}
+        <span> Maciej Miąso </span>
+        CuValley Hack 2023
+      </h5>
+    </Wrapper>
+  )
 }
 
 export default Footer
@@ -11,5 +19,11 @@ const Wrapper = styled.footer`
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid red;
+  margin: 5px;
+  border: 2px #de824e solid;
+  border-radius: 15px;
+  padding: 15px;
+  span {
+    color: #de824e;
+  }
 `
